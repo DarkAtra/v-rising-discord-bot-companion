953026285edfa8641fdb01c65f5513dce89767f5
DarkAtra <darkatra@gmail.com>
2024-06-01 17:38:16 +0200

    feat: basic auth for all api endpoints

---
+++ PluginConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace v_rising_discord_bot_companion;
+
+public readonly record struct PluginConfig(
+    List<BasicAuthUser> BasicAuthUsers
+);
+
+public readonly record struct BasicAuthUser(
+    string Username,
+    string Password
+);
