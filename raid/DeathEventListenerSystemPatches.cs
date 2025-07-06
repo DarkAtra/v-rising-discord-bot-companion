52e052f200dfc50758309b5ac634840d13b0bb97
Tommy Schmidt <darkatra@gmail.com>
2025-07-06 20:52:38 +0200

    feat: improve raid detection (#34)
    * see: https://github.com/DarkAtra/v-rising-discord-bot/issues/146

--- raid/DeathEventListenerSystemPatches.cs
+++ raid/DeathEventListenerSystemPatches.cs
@@ -18,51 +18,39 @@ public class DeathEventListenerSystemPatches {
     private static readonly PrefabGUID SIEGE_GOLEM_PREFAB_ID = new(914043867);
     private static ServerGameManager? serverGameManager;
 
-    private static readonly List<Raid> _raids = [];
+    private static readonly Dictionary<ulong, Raid> _raids = new();
 
     public static List<Raid> getRaids() {
         removeExpiredRaids();
-        return _raids;
+        return _raids.Select(it => it.Value).ToList();
     }
 
     private static void removeExpiredRaids() {
-        _raids.RemoveAll(raid => DateTime.UtcNow > raid.Occurred.AddMinutes(10));
+        List<ulong> expiredRaids = new();
+        foreach (var keyValuePair in _raids) {
+            if (DateTime.UtcNow > keyValuePair.Value.Updated.AddMinutes(10)) {
+                expiredRaids.Add(keyValuePair.Key);
+            }
+        }
+        foreach (var expiredRaid in expiredRaids) {
+            _raids.Remove(expiredRaid);
+        }
     }
 
     [HarmonyPostfix]
     [HarmonyPatch(nameof(DeathEventListenerSystem.OnUpdate))]
-    static void Postfix(DeathEventListenerSystem __instance) {
+    public static void Postfix(DeathEventListenerSystem __instance) {
 
         var deathEvents = __instance._DeathEventQuery.ToComponentDataArray<DeathEvent>(Allocator.Temp);
         try {
-            foreach (DeathEvent deathEvent in deathEvents) {
+            foreach (var deathEvent in deathEvents) {
 
                 if (VWorld.Server.EntityManager.HasComponent<AnnounceCastleBreached>(deathEvent.Died)
                     && deathEvent.StatChangeReason.Equals(StatChangeReason.StatChangeSystem_0)) {
 
                     // if a player with siege golem buff breached the castle
                     if (getServerGameManager().TryGetBuff(deathEvent.Killer, SIEGE_GOLEM_PREFAB_ID.ToIdentifier(), out _)) {
-
-                        var attackingPlayer = VPlayer.from(VWorld.Server.EntityManager.GetComponentData<PlayerCharacter>(deathEvent.Killer).UserEntity);
-
-                        var attackers = new List<VPlayer>();
-                        attackers.Add(attackingPlayer);
-                        attackers.AddRange(getClanMembers(attackingPlayer));
-
-                        var castleHeartEntity = VWorld.Server.EntityManager.GetComponentData<CastleHeartConnection>(deathEvent.Died).CastleHeartEntity._Entity;
-                        var defendingPlayer = VPlayer.from(VWorld.Server.EntityManager.GetComponentData<UserOwner>(castleHeartEntity).Owner._Entity);
-
-                        var defenders = new List<VPlayer>();
-                        defenders.Add(defendingPlayer);
-                        defenders.AddRange(getClanMembers(defendingPlayer));
-
-                        _raids.Add(
-                            new Raid(
-                                Attackers: attackers.Select(map).ToList(),
-                                Defenders: defenders.Select(map).ToList(),
-                                Occurred: DateTime.UtcNow
-                            )
-                        );
+                        onCastleBreached(deathEvent);
                     }
                 }
             }
@@ -75,11 +63,71 @@ public class DeathEventListenerSystemPatches {
         removeExpiredRaids();
     }
 
-    private static Player map(VPlayer player) {
+    private static void onCastleBreached(DeathEvent deathEvent) {
+
+        var attackingPlayer = VPlayer.from(VWorld.Server.EntityManager.GetComponentData<PlayerCharacter>(deathEvent.Killer).UserEntity);
+        var castleHeartEntity = VWorld.Server.EntityManager.GetComponentData<CastleHeartConnection>(deathEvent.Died).CastleHeartEntity._Entity;
+        var defendingPlayer = VPlayer.from(VWorld.Server.EntityManager.GetComponentData<UserOwner>(castleHeartEntity).Owner._Entity);
+
+        var attackers = new List<VPlayer>();
+        attackers.Add(attackingPlayer);
+        attackers.AddRange(getClanMembers(attackingPlayer));
+
+        var defenders = new List<VPlayer>();
+        defenders.Add(defendingPlayer);
+        defenders.AddRange(getClanMembers(defendingPlayer));
+
+        var castleOwnerId = defendingPlayer.GetId(); // might be better to use the castle hearth as id here
+        var now = DateTime.UtcNow;
+
+        if (!_raids.TryGetValue(castleOwnerId, out var raid)) {
+
+            var raidId = Guid.NewGuid();
+
+            Plugin.Logger.LogDebug($"New raid with id '{raidId}' started for castle owned by player with id '{castleOwnerId}'...");
+
+            _raids.Add(
+                castleOwnerId,
+                new Raid(
+                    Id: raidId,
+                    Attackers: attackers.Select(it => map(it, now)).ToList(),
+                    Defenders: defenders.Select(it => map(it, now)).ToList(),
+                    Occurred: now,
+                    Updated: now
+                )
+            );
+
+            Plugin.Logger.LogDebug($"Added new raid with id '{raid.Id}'.");
+            return;
+        }
+
+        Plugin.Logger.LogDebug($"Updating raid with id '{raid.Id}' if necessary...");
+
+        foreach (var vPlayer in attackers) {
+            if (raid.Attackers.All(it => it.Id != vPlayer.GetId())) {
+                Plugin.Logger.LogDebug($"New attacker joined the raid with id '{raid.Id}'. Setting raid.Updated to: {now}");
+                raid.Attackers.Add(map(vPlayer, now));
+                raid.Updated = now;
+            }
+        }
+        foreach (var vPlayer in defenders) {
+            if (raid.Defenders.All(it => it.Id != vPlayer.GetId())) {
+                Plugin.Logger.LogDebug($"New defender joined the raid with id '{raid.Id}'. Setting raid.Updated to: {now}");
+                raid.Defenders.Add(map(vPlayer, now));
+                raid.Updated = now;
+            }
+        }
+
+        Plugin.Logger.LogDebug($"Finished updating raid with id '{raid.Id}'.");
+    }
+
+    private static Player map(VPlayer player, DateTime now) {
         return new Player(
+            Id: player.GetId(),
             Name: ((VCharacter) player.VCharacter!).Character.Name.ToString(),
             GearLevel: ((VCharacter) player.VCharacter!).getGearLevel(),
-            Clan: player.VUser.GetClanName()
+            Clan: player.VUser.GetClanName(),
+            JoinedAt: now
         );
     }
 
@@ -90,7 +138,7 @@ public class DeathEventListenerSystemPatches {
 
         if (VWorld.Server.EntityManager.HasComponent<ClanTeam>(clanEntity)) {
             var users = VWorld.Server.EntityManager.GetBuffer<SyncToUserBuffer>(clanEntity);
-            foreach (SyncToUserBuffer user in users) {
+            foreach (var user in users) {
                 if (user.UserEntity != player.VUser.UserEntity) {
                     clanMembers.Add(VPlayer.from(user.UserEntity));
                 }
--- raid/Player.cs
+++ raid/Player.cs
@@ -1,7 +1,11 @@
-﻿namespace v_rising_discord_bot_companion.raid;
+﻿using System;
+
+namespace v_rising_discord_bot_companion.raid;
 
 public readonly record struct Player(
+    ulong Id,
     string Name,
     int GearLevel,
-    string? Clan
+    string? Clan,
+    DateTime JoinedAt
 );
